b028e429c7b45694011bf380adaf15cfa183eb14
mrcar0678 <duck104032@yahoo.com.tw>
2025-06-11 23:59:42 +0800

    修正 config.environment.rb 中誤用 config 導致部署錯誤

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # Load the Rails application.
 require_relative 'application'
-config.web_console.whitelisted_ips = '0.0.0.0/0'
+
 
 # Initialize the Rails application.
 Rails.application.initialize!
