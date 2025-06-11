67eaad785183355acb066019c53ebbb824d44281
mrcar0678 <duck104032@yahoo.com.tw>
2025-06-11 23:50:18 +0800

    修正 web_console 錯誤：移除 application.rb 中的 web_console 設定

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Kamigo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.web_console.whitelisted_ips = '147.92.149.170'
+    #config.web_console.whitelisted_ips = '147.92.149.170'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
