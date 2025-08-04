490dba1b820a4b0e924087d7be9ca2a481f2228d
mrcar0678 <88051344+mrcar0678@users.noreply.github.com>
2025-08-04 17:59:22 +0800

    Update event_record.rb

--- app/models/event_record.rb
+++ app/models/event_record.rb
@@ -1,6 +1,3 @@
 class EventRecord < ApplicationRecord
-<<<<<<< HEAD
-=======
-	validates :event_id, uniqueness: true
->>>>>>> 6248f8d (初始化專案，上傳 LINE Bot 和爬蟲功能)
+  validates :event_id, uniqueness: true
 end
