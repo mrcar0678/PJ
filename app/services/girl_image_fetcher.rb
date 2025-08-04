b597a2baf4e4e1dbaa583adf486e92770a136836
mrcar0678 <88051344+mrcar0678@users.noreply.github.com>
2025-08-04 19:49:39 +0800

    Update girl_image_fetcher.rb

--- app/services/girl_image_fetcher.rb
+++ app/services/girl_image_fetcher.rb
@@ -5,7 +5,20 @@ class GirlImageFetcher
     'https://i.meee.com.tw/NSaXGcZ.jpg',
     'https://i.meee.com.tw/oxfgmCQ.jpg',
     'https://i.meee.com.tw/zGjlaFO.jpg',
-    'https://i.meee.com.tw/ttz54fZ.jpg'
+    'https://i.meee.com.tw/ttz54fZ.jpg',
+    'https://i.meee.com.tw/cSJhvJN.jpg',
+    'https://i.meee.com.tw/r8KCl8K.jpg',
+    'https://i.meee.com.tw/EhHWTiU.jpg',
+    'https://i.meee.com.tw/UrAlRxj.jpg',
+    'https://i.meee.com.tw/TM9Xx0a.jpg',
+    'https://i.meee.com.tw/vvcxysM.jpg',
+    'https://i.meee.com.tw/sIpdfWW.jpg',
+    'https://i.meee.com.tw/UTOPyIc.jpg',
+    'https://i.meee.com.tw/j1UoxTv.jpg',
+    'https://i.meee.com.tw/DH2RvUG.jpg',
+    'https://i.meee.com.tw/53Y9baU.jpg',
+    'https://i.meee.com.tw/MLDIwKC.jpg',
+    'https://i.meee.com.tw/6AfywgO.jpg'
     
     # 可自行增加更多圖片連結
   ]
